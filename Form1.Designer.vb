d80ca19b550fcafb94e961d84a3eec7896074f29
marwan eldesouki <37198610+marwaneldesouki@users.noreply.github.com>
2020-01-19 01:35:51 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(282, 29)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "URI Solver By Deso"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 10.0!)
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(264, 5)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "X"
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 10.0!)
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(242, 7)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(25, 17)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "--"
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.ForeColor = System.Drawing.Color.Red
+        Me.Label4.Location = New System.Drawing.Point(-3, 17)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(329, 17)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "________________________________________"
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(9, 360)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 27)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Solve"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.Silver
+        Me.TextBox1.Location = New System.Drawing.Point(78, 328)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(118, 24)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "Problem ID?"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Black
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.ForeColor = System.Drawing.Color.Silver
+        Me.TextBox2.Location = New System.Drawing.Point(6, 38)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(270, 266)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.Silver
+        Me.Label5.Location = New System.Drawing.Point(6, 309)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(68, 17)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Status: ..."
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(0, 393)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(282, 33)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "About"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.Red
+        Me.Button3.Location = New System.Drawing.Point(143, 360)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(133, 27)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(282, 428)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label4)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "URI Solver By Deso"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,142 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+Imports System.Threading
+
+Public Class Form1
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+    Function check(str As String)
+        Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create("https://mamun-amin.blogspot.com/p/uri.html"), HttpWebRequest)
+        httpWebRequest.Proxy = Nothing
+        Using streamReader As StreamReader = New StreamReader(httpWebRequest.GetResponse().GetResponseStream())
+            Dim prompt As String = streamReader.ReadToEnd()
+            If (prompt.Contains(str.ToString)) Then
+                Label5.ForeColor = Color.Green
+                Label5.Text = "Status: Your Code is Founded"
+                Me.Refresh()
+                Thread.Sleep(200)
+                Label5.ForeColor = Color.Red
+                Label5.Text = "Status: Wait> Finding The Answer"
+                grab(str)
+            Else
+                MessageBox.Show("Your Code Is Not exsist in our DB" + vbCrLf + "Plz for Your code again", "URI Solver")
+                Label5.Text = "Status: Not Found"
+
+            End If
+
+        End Using
+    End Function
+
+    Function grab(strx As String)
+        Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create("https://mamun-amin.blogspot.com/search?q=" + strx), HttpWebRequest)
+        httpWebRequest.Proxy = Nothing
+        Using streamReader As StreamReader = New StreamReader(httpWebRequest.GetResponse().GetResponseStream())
+            Dim prompt As String = streamReader.ReadToEnd()
+            regx(prompt)
+
+        End Using
+    End Function
+    Function regx(xstr As String)
+
+        Dim r As New Regex("<a href='(.*?)'>Solution of URI " + TextBox1.Text)
+        Dim matchess As MatchCollection = r.Matches(xstr)
+
+        For Each itemcode As Match In matchess
+            ans(itemcode.Groups(1).Value)
+        Next
+
+
+
+
+
+    End Function
+    Function ans(str As String)
+        Dim httpWebRequest As HttpWebRequest = CType(WebRequest.Create(str), HttpWebRequest)
+        httpWebRequest.Proxy = Nothing
+        Using streamReader As StreamReader = New StreamReader(httpWebRequest.GetResponse().GetResponseStream())
+            Dim prompt As String = streamReader.ReadToEnd()
+            regxans(prompt.ToString)
+
+        End Using
+    End Function
+    Dim zz As String
+    Function regxans(str As String)
+        Dim r As New Regex("<pre class=""c"" name=""code"">([^<]*)</pre>")
+        Dim matchess As MatchCollection = r.Matches(str)
+
+        For Each itemcode As Match In matchess
+            zz = itemcode.Groups(1).Value
+
+
+        Next
+        zz = zz.Replace("amp;", "")
+        zz = zz.Replace("&lt;", "<")
+        zz = zz.Replace("&gt;", ">")
+        zz = zz.Replace("/**Bismillahir Rahmanir Rahim.**/", "")
+
+
+        TextBox2.Text = zz
+        Label5.Text = "Status: Complete"
+        Label5.ForeColor = Color.Green
+    End Function
+    Dim xx As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox2.Clear()
+
+        If (TextBox1.Text = "" Or TextBox1.Text = "Problem ID?") Then
+            MessageBox.Show("Fill The Box Plz")
+        Else
+            Label5.ForeColor = Color.Red
+            Label5.Text = "Status: Wait >Checking Your Code"
+
+
+            check(TextBox1.Text)
+        End If
+
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        End
+    End Sub
+    Private isMouseDown As Boolean
+    Private mouseOffset As Point
+    Private Sub Label1_MouseDown(sender As Object, e As MouseEventArgs) Handles Label1.MouseDown
+        Dim flag As Boolean = e.Button = MouseButtons.Left
+        If flag Then
+
+            Me.mouseOffset = New Point(0 - e.X, 0 - e.Y)
+            Me.isMouseDown = True
+        End If
+    End Sub
+
+    Private Sub Label1_MouseMove(sender As Object, e As MouseEventArgs) Handles Label1.MouseMove
+        Dim flag As Boolean = Me.isMouseDown
+        If flag Then
+            Dim mousePosition As Point = Control.MousePosition
+            mousePosition.Offset(Me.mouseOffset.X, Me.mouseOffset.Y)
+            Me.Location = mousePosition
+        End If
+    End Sub
+
+    Private Sub Label1_MouseUp(sender As Object, e As MouseEventArgs) Handles Label1.MouseUp
+        Dim flag As Boolean = e.Button = MouseButtons.Left
+        If flag Then
+            Me.isMouseDown = False
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox2.Clear()
+        TextBox1.Text = "Problem ID?"
+        Label5.Text = "Status: ..."
+        Label5.ForeColor = Color.Silver
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Coded by Deso" + vbCr + "IG: Marwaneldesouki" + vbCrLf + "GITHUB: Marwaneldesouki", "About")
+    End Sub
+End Class
